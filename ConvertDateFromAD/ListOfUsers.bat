490541fe458f7720e3adc5856586088e02473cd3
Lubos Saloky <35326016+lsaloky@users.noreply.github.com>
2019-04-16 19:37:42 +0200

    Updated script to convert date exported from AD: from numeric to date format

--- ConvertDateFromAD/ListOfUsers.bat
+++ ConvertDateFromAD/ListOfUsers.bat
@@ -1,5 +1,5 @@
-csvde -f Users.txt -d "OU=Users,OU=KOS,OU=EUR,DC=molex,DC=com" -r "(objectClass=user)" -l "lastLogon"
-ldifde -d OU=Users,OU=KOS,OU=EUR,dc=molex,dc=com -l lastLogon -r "(ObjectCategory=user)" -f Users.txt
+csvde -f Users.txt -d "OU=Users,DC=domain,DC=com" -r "(objectClass=user)" -l "lastLogon"
+ldifde -d OU=Users,dc=domain,dc=com -l lastLogon -r "(ObjectCategory=user)" -f Users.txt
 pause
 
 
