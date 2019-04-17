96ccb0bc796dc8a15cd1373f96bd1a1e4f58e554
Lubos Saloky <Lubos.Saloky@ptv.com>
2019-04-17 08:18:28 +0200

    Added script to create a 1 GB file

---
+++ Create1GBFile/Create 1GB Dump File.bat
@@ -0,0 +1,34 @@
+@echo off
+copy a.txt+a.txt b.txt
+copy b.txt+b.txt a.txt
+copy a.txt+a.txt b.txt
+copy b.txt+b.txt a.txt
+copy a.txt+a.txt b.txt
+copy b.txt+b.txt a.txt
+copy a.txt+a.txt b.txt
+copy b.txt+b.txt a.txt
+copy a.txt+a.txt b.txt
+copy b.txt+b.txt a.txt
+copy a.txt+a.txt b.txt
+copy b.txt+b.txt a.txt
+copy a.txt+a.txt b.txt
+copy b.txt+b.txt a.txt
+copy a.txt+a.txt b.txt
+copy b.txt+b.txt a.txt
+copy a.txt+a.txt b.txt
+copy b.txt+b.txt a.txt
+copy a.txt+a.txt b.txt
+copy b.txt+b.txt a.txt
+copy a.txt+a.txt b.txt
+copy b.txt+b.txt a.txt
+copy a.txt+a.txt b.txt
+copy b.txt+b.txt a.txt
+copy a.txt+a.txt b.txt
+copy b.txt+b.txt a.txt
+copy a.txt+a.txt b.txt
+copy b.txt+b.txt a.txt
+copy a.txt+a.txt b.txt
+copy b.txt+b.txt a.txt
+del b.txt
+
+
---
+++ Create1GBFile/README.md
@@ -0,0 +1,5 @@
+# Create 1GB Dump File
+
+Script creates a 1 GB file. Cannot find any useful purpose for this script, except overwriting deleted data on the disk drive.
+
+Execute "Create 1GB Dump File.bat". Do not execute twice.
